b8a642ea635c1ea8baca73540480cbeefa5618bd
moussaballa <cifadel1@hotmail.com>
2016-12-19 22:30:00 -0500

    les test unitaire

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,7 +3,7 @@ require 'contacts/contact'
 
 module Contacts
 describe Contact do
-  it "un contact cree " do
+  it "retourne un contact complet avec tout ses attributs  " do
     adresse = Adresse.new( 100, "Sherbrook", "h3h-2L7", "Montréal","Canada" )
     adresses =[adresse]
     tel = Telephone.new(1,"5142546369", :Mobile)
@@ -11,7 +11,7 @@ describe Contact do
     soc = Social.new( "moisebala", :facebook)
     socs = [soc]
     attendu = Contact.new( "Moussa","Balla", tels,"moisebala@gmail.com",
-                            adresses,"UQAM",:Famille,"03,16,1988",socs)
+                            adresses,"UQAM",:Famille,"03-16-1988",socs)
     attendu.to_s.must_equal
     "Moussa Balla
     Téléphones :  +1 514-254-6369 (Mobile)
@@ -23,19 +23,19 @@ describe Contact do
     sociaux : @moisebala  (Facebook)"
   end
   describe Telephone do
-    it "Ajouter un numero de telephone pour un contact" do
+    it "retourne un numero de telephone pour un contact" do
       c = Telephone.new (1,"5146035652","Mobile")
       c.to_s.must_equal "+1 514-603-5652 (Mobile)"
     end
   end
   describe Adresse  do
-    it "Ajouter une adresse pour un contact " do
+    it "retourne  une adresse pour un contact " do
       c = Adresse.new (1,"joliette","J4h-LL3","Montreal","Canada")
       c.to_s.must_equal "1, joliette (J4h-LL3) -  Montreal ,  Canada"
     end
   end
    describe Social do
-    it "Ajouter les reseaux sociaux pour un contact " do
+    it "retourne les reseaux sociaux pour un contact " do
       c = SociaL.new ("moisebala","Facebook")
       c.to_s.must_equal "@moisebala  (Facebook)"
     end
